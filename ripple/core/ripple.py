532de5f0eab39ae09c69fe38eb2a928d22fa97db
dcambur <dcambur@gmail.com>
2023-03-25 14:58:01 +0200

    update to follow persistence class

--- ripple/core/ripple.py
+++ ripple/core/ripple.py
@@ -1,19 +1,25 @@
 import threading
 
-from ripple.core import persist_const
+from ripple.core import const
 from ripple.core.persistance import RipplePersist
 
 
 class RippleDB:
-    def __init__(self, persist_as=persist_const.NONE):
-        self.__write = "+"
-        self.__delete = "-"
-        self.__persist_manager = RipplePersist(persist_as)
+    def __init__(self, persist_as=const.NONE):
+
+        self.persist_as = persist_as
+        self.__persist_manager = RipplePersist(self.persist_as)
         self.data = self.__persist_manager.sync_db(dict())
+
+    def persist(self, command):
+        if self.persist_as == const.SNAPSHOT:
+            self.__persist_manager.model.snapshot_write(self.data)
+        if self.persist_as == const.AOF:
+            self.__persist_manager.model.aof_write(command[0], command[1], command[2])
 
     def create(self, key, value):
         self.data[key] = value
-        self.__persist_manager.aof_write(key, value, self.__write)
+        self.persist([key, value, const.WRITE])
 
     def read(self, key):
         if key not in self.data.keys():
@@ -23,10 +29,13 @@ class RippleDB:
     def delete(self, key):
         if key not in self.data.keys():
             return None
+
+        tmp_record = [key, self.data[key], const.DELETE]
         del self.data[key]
-        self.__persist_manager.aof_write(key, self.data[key], self.__delete)
+        self.persist(tmp_record)
+
         return True
 
     def create_snapshot(self):
-        thread = threading.Thread(target=self.__persist_manager.snapshot_write, args=(self.data,))
+        thread = threading.Thread(target=self.__persist_manager.model.snapshot_write, args=(self.data,))
         thread.start()
