ba74e89bf610f6b5f8aa03de23b4dc4291024477
dcambur <dcambur@gmail.com>
2023-03-26 20:48:06 +0300

    thread lock during CRUD

--- ripple/core/db.py
+++ ripple/core/db.py
@@ -1,34 +1,38 @@
 import threading
-from .meta import BackupInfo
+from .meta import BackupInfo, OperatorTypes, NONEInfo
 from .persistance import Persistence
 
 
 class RippleDB:
-    def __init__(self, persist_info=BackupInfo):
+    def __init__(self, persist_info=NONEInfo()):
         self.persist_info = persist_info
         self.persist_manager = Persistence(persist_info)
         self.data = self.persist_manager.load_backup()
+        self.lock = threading.Lock()
 
     def create(self, key, value):
-        self.data[key] = value
-        record = [key, self.data[key], self.persist_info.WRITE]
-        self.persist_manager.persist(record)
+        with self.lock:
+            self.data[key] = value
+            record = [key, self.data[key], OperatorTypes.WRITE]
+            self.persist_manager.persist(record)
 
     def read(self, key):
-        if key not in self.data.keys():
-            return None
-        return self.data[key]
+        with self.lock:
+            if key not in self.data:
+                return None
+            return self.data[key]
 
     def delete(self, key):
-        if key not in self.data.keys():
-            return None
 
-        record = [key, self.data[key], self.persist_info.DELETE]
-        del self.data[key]
+        with self.lock:
+            if key not in self.data:
+                return None
 
-        self.persist_manager.persist(record)
+            record = [key, self.data[key], OperatorTypes.DELETE]
+            del self.data[key]
+            self.persist_manager.persist(record)
 
-        return True
+            return True
 
     def create_snapshot(self):
         thread = threading.Thread(target=self.persist_manager.backup.write, args=(self.data,))
