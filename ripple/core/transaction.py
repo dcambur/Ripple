a40a14ca57a454a0e255d7cfe2a46b728eea8ba5
dcambur <dcambur@gmail.com>
2023-03-26 19:58:43 +0300

    import of meta.py constants for write/delete type definiton

--- ripple/core/transaction.py
+++ ripple/core/transaction.py
@@ -1,6 +1,5 @@
 import threading
-
-from . import const
+from .meta import OperatorTypes
 
 
 class Transaction:
@@ -10,14 +9,14 @@ class Transaction:
         self.lock = threading.Lock()
 
     def add(self, key, value):
-        self.transaction_store.append([key, value, const.WRITE])
+        self.transaction_store.append([key, value, OperatorTypes.WRITE])
 
     def __commit(self):
         with self.lock:
             for command in self.transaction_store:
-                if command[2] == const.WRITE:
+                if command[2] == OperatorTypes.WRITE:
                     self.db.create(command[0], command[1])
-                if command[2] == const.DELETE:
+                if command[2] == OperatorTypes.DELETE:
                     self.db.delete(command[0], command[1])
         self.transaction_store = []
 
