9fa71fda4990f3c0a26757fb5e9c86aaddfdc85d
dcambur <dcambur@gmail.com>
2023-03-26 20:50:21 +0300

    changed general flow for backup system; set up persistance with setting objects from meta.py

--- ripple/core/persistance.py
+++ ripple/core/persistance.py
@@ -1,94 +1,100 @@
 import os
 import json
-
-from ripple.core import const
+from .meta import BackupTypes, OperatorTypes
 
 
 class AOF:
-    def __init__(self, name="ripple"):
-        self.name = name + ".adb"
-        self.__write = "+"
-        self.__delete = "-"
-        self.__aof_separator = ":"
-        self.__line_separator = "\n"
+    def __init__(self, persist_info):
+        self.persist_info = persist_info
         self.__create_file()
 
     def __create_file(self):
-        if not self.__find_aof():
-            open(self.name, "x").close()
+        if not self.__find():
+            open(self.persist_info.full_path, "x").close()
 
-    def __find_aof(self):
-        return os.path.exists(self.name)
+    def __find(self):
+        return os.path.exists(self.persist_info.full_path)
 
-    def aof_load(self, load_dict):
+    def write(self, key, value, op):
+        with open(self.persist_info.full_path, "a") as write_desc:
+            write_desc.write(f"{op}:{key}:{json.dumps(value)}\n")
+
+    def load(self):
+        load_dict = {}
         # line[0] -> operator; line[1] -> key, line[2] -> value
-        with open(self.name, "r") as read_desc:
+        with open(self.persist_info.full_path, "r") as read_desc:
             for line in read_desc:
-                line = line.strip(self.__line_separator).split(self.__aof_separator, 2)
-                if line[0] == self.__write:
+                line = line.strip(self.persist_info.LINE_SEPARATOR).split(self.persist_info.DATA_SEPARATOR, 2)
+                if line[0] == OperatorTypes.WRITE:
                     load_dict[line[1]] = json.loads(line[2])
-                if line[0] == self.__delete:
+                if line[0] == OperatorTypes.DELETE:
                     del load_dict[line[1]]
         return load_dict
 
-    def aof_write(self, key, value, op):
-        with open(self.name, "a") as write_desc:
-            write_desc.write(f"{op}:{key}:{json.dumps(value)}\n")
-
 
 class Snapshot:
-    def __init__(self, name, save_after=5):
-        self.__snapshot_name = name + ".sdb"
-        self.__create_file()
-        self.save_after = save_after
+    def __init__(self, persist_info):
+        self.to_write = {}
+        self.persist_info = persist_info
         self.count = 0
 
+        self.__create_file()
+
     def __create_file(self):
-        if not self.__find_snapshot():
-            with open(self.__snapshot_name, "w") as write_desc:
+        if not self.__find():
+            with open(self.persist_info.full_path, "w") as write_desc:
                 write_desc.write("{}")
 
-    def __find_snapshot(self):
-        return os.path.exists(self.__snapshot_name)
+    def __find(self):
+        return os.path.exists(self.persist_info.full_path)
 
-    def snapshot_write(self, data_dict):
+    def __update_snapshot(self, key, value, op):
+        if op == OperatorTypes.WRITE:
+            self.to_write[key] = value
+        if op == OperatorTypes.DELETE:
+            del self.to_write[key]
+
+        self.count += 1
+
+    def write(self, key, value, op):
+        self.__update_snapshot(key, value, op)
+
         if self.count_tick():
-            with open(self.__snapshot_name, "w") as write_desc:
-                json.dump(data_dict, write_desc)
+            with open(self.persist_info.full_path, "w") as write_desc:
+                json.dump(self.to_write, write_desc)
+                self.count = 0
 
-    def snapshot_load(self):
-        with open(self.__snapshot_name, "r") as read_desc:
+    def load(self):
+        with open(self.persist_info.full_path, "r") as read_desc:
             return json.load(read_desc)
 
     def count_tick(self):
-        self.count += 1
-        if self.count == self.save_after:
-            self.count = 0
+        if self.count == self.persist_info.save_every:
             return True
         return False
 
 
-class RipplePersist:
-    def __init__(self, persist_as, name="ripple"):
-        self.__name = name
-        self.__persist_as = persist_as
-        self.model = self.__get_model()
+class Persistence:
+    def __init__(self, persist_info):
+        self.persist_info = persist_info
+        self.backup = self.__get_model()
+
+    def persist(self, command):
+        if self.backup:
+            self.backup.write(command[0], command[1], command[2])
 
     def __get_model(self):
-        match self.__persist_as:
-            case const.NONE:
+        match self.persist_info.model_type:
+            case BackupTypes.NONE:
                 return None
-            case const.AOF:
-                return AOF(self.__name)
-            case const.SNAPSHOT:
-                return Snapshot(self.__name)
+            case BackupTypes.AOF:
+                return AOF(self.persist_info)
+            case BackupTypes.SNAPSHOT:
+                return Snapshot(self.persist_info)
 
-    def sync_db(self, data_dict):
-        match self.__persist_as:
-            case const.NONE:
-                return data_dict
-            case const.AOF:
-                return self.model.aof_load(data_dict)
-            case const.SNAPSHOT:
-                return self.model.snapshot_load()
-        return data_dict
+    def load_backup(self):
+        match self.persist_info.model_type:
+            case BackupTypes.NONE:
+                return {}
+            case _:
+                return self.backup.load()
